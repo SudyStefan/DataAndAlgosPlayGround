2eb18dc7160666aed3da0e18f77eb09aafb0b412
BergMichel <stefan.sudy1@gmail.com>
2026-03-11 08:57:48 +0100

    converted css to tailwind

--- landing_page/src/components/App.tsx
+++ landing_page/src/components/App.tsx
@@ -1,18 +1,22 @@
 import { useEffect, useState } from "react";
-import "../styles//App.css";
+import "../styles/App.css";
 import SiteCard from "./SiteCard";
 
 export type Site = {
   name: string;
   description: string;
-  logoPath: string;
+  logoName: string;
   indexPath: string;
 };
 
-function App() {
+type AppConfig = {
+  sites: Site[];
+};
+
+const App = () => {
   const [sites, setSites] = useState<Site[]>([]);
 
-  const loadConfig = async (): Promise<any> => {
+  const loadConfig = async (): Promise<AppConfig> => {
     const response = await fetch("./sites.json");
     if (!response.ok) {
       throw new Error("Failed to fetch sites.json!");
@@ -29,17 +33,19 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div className="siteContainer">
-        <h1>Available Apps</h1>
-        <div className="cardGrid">
+    <div className="flex min-h-screen min-w-screen items-center justify-center bg-stone-800">
+      <div className="flex-row">
+        <h1 className="pb-5 text-center text-5xl font-bold text-slate-400">
+          Available Apps
+        </h1>
+        <div className="mx-auto grid w-[90%] grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-5">
           {sites.map((site) => (
             <SiteCard key={site.name} {...site} />
           ))}
         </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default App;
--- landing_page/src/components/SiteCard.tsx
+++ landing_page/src/components/SiteCard.tsx
@@ -1,18 +1,33 @@
 import type { Site } from "./App";
+import { cn } from "../helpers/utils";
 
-const SiteCard = ({ name, description, logoPath, indexPath }: Site) => {
+const SiteCard = ({ name, description, logoName, indexPath }: Site) => {
   return (
     <a className="siteRef" href={indexPath}>
-      <div className="siteCard">
-        <div className="siteHeader">
-          <h1>{name}</h1>
+      <div
+        className={cn(
+          "flex h-full flex-col items-center justify-center",
+          "rounded-xl border border-amber-100 bg-stone-600",
+          "p-5 shadow-sm",
+          "transition-all duration-200 ease-in-out",
+          "hover:scale-105 hover:shadow-lg",
+        )}
+      >
+        <div className="flex min-h-[20%]">
+          <h1 className="pb-5 text-center text-2xl font-bold text-slate-400">
+            {name}
+          </h1>
         </div>
-        <div className="siteBody">
-          <div className="siteLogo">
-            <img alt={`${name} logo`} src={logoPath} />
+        <div className="flex w-full grow flex-row flex-wrap justify-center">
+          <div className="grow basis-2/5">
+            <img
+              alt={`${name} Logo`}
+              src={logoName}
+              className="h-auto max-w-full rounded-lg"
+            />
           </div>
-          <div className="siteDesc">
-            <p>{description}</p>
+          <div className="grow basis-3/5">
+            <p className="font-semibold text-slate-400">{description}</p>
           </div>
         </div>
       </div>
