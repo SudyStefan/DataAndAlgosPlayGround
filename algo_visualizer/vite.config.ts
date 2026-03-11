e1fa9b683d7736a590ec638c3696d8833005b37a
Stefan Sudy <stefan.sudy@edu.fh-joanneum.at>
2026-03-11 11:50:35 +0100

    improved building

--- algo_visualizer/vite.config.ts
+++ algo_visualizer/vite.config.ts
@@ -2,4 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({ plugins: [react()], base: "" });
+export default defineConfig({
+  plugins: [react()],
+  base: "",
+  build: { outDir: "../dist/algo_visualizer", emptyOutDir: true }
+});
